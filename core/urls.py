05728e347b1916700ba537dac18385abb5ebe24f
Wei Zheng <wzyeo1990@gmail.com>
2020-07-11 00:18:46 +0800

    Modified url regex

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from .views import UrlRedirectView
 
 app_name = 'core'
 urlpatterns = [
-    re_path(r'^(?P<origin_url>[a-zA-Z0-9\/.% ]+)$', UrlRedirectView.as_view(), name='redirect-view')
+    re_path(r'^(?P<origin_url>.*)$', UrlRedirectView.as_view(), name='redirect-view')
 ]
