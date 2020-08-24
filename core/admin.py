aab88609d984a58562603fe0abcb453bb4ba6b91
Wei Zheng <weizheng@netvirta.com>
2020-08-24 13:24:25 +0800

    Added custom list_display & modified timezone

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from core.models import UrlMapping
 
-admin.site.register(UrlMapping)
+class UrlMappingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'origin_url', 'dest_url')
+
+admin.site.register(UrlMapping, UrlMappingAdmin)
